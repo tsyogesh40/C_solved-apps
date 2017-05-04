991d78eb97972407f5305743cde3fed4cb6bfc44
Yogesh T S <tsyogesh40@gmail.com>
2017-05-04 08:39:41 -0700

    Update Pattern Printing - Numbers [ZOHO].c
    Error rectifies

--- Pattern Printing - Numbers [ZOHO].c
+++ Pattern Printing - Numbers [ZOHO].c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main()
 {
-int i,j,n,x=1,y=1;
-scanf("%d",&n);
-int z,a=0,k=0;
-for(i=n;i>0;i--)
+  int i,j,n,y=1,x=1;
+  scanf("%d",&n);
+  int z,a=0,k=0;
+ for(i=n;i>0;i--){
+     z=n;
+     k=y;
+    printf("%d ",y);
+ for(j=1;j<i;j++)
     {
-        z=n;
-        k=y;
-        printf("%d ",y);
-    for(j=1;j<i;j++)
-            {   
-                if(j>0)
-                    a+=k+z;
-                    printf("%d ",a);
-                z--;
-                k=0;
-            }
-    printf("\n");
-    k=y++;
-    a=0;
+        if(j>0)
+        {
+         a+=k+2+j;
+         printf("%d ",a);
+        }
+        z--;
+     k=0;
     }
-return 0;
+     printf("\n");
+     
+     k=y++;
+     a=0;
+     }
+
+    return 0; 
+
 }
